7f566ecc97824dd4ec9c0fcf99ae6dca9e8fc02c
moumou <hielf@qq.com>
2017-11-01 23:50:32 +0800

    init commit

---
+++ config/schedule.rb
@@ -0,0 +1,35 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+# !!!!!!!!!
+# ADD PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin to crontab -e
+
+# every 1.hour do
+#   rake "get_data:spyder"
+# end
+
+every 5.minutes do
+  rake "download_video:publish"
+end
+
+every 3.minutes do
+  rake "download_video:download"
+end
