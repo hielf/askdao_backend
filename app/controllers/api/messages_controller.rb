de3c16c8c5524110a2d52e2f837d2b95decdce1e
moumou <hielf@qq.com>
2017-11-14 17:19:30 +0800

    adjust api controllers

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,14 @@
+class Api::MessagesController < Api::BaseController
+
+    def get_message
+      user = User.find_or_create_by(open_id: params[:openid])
+      message = Message.create(user_id: user.id, content: params[:content])
+
+      if message.save
+        render_json(0, '创建成功')
+      else
+        render_json(1)
+      end
+    end
+
+end
