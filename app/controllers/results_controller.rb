e0a1cfbce391e66cd0a75b1da8e0eba75a4e4953
moumou <hielf@qq.com>
2017-11-16 05:12:32 +0800

    edit view

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,8 +2,9 @@ class ResultsController < ApplicationController
 
   def videos
     spyder_id = params[:spyder_id]
-    @res = HTTParty.get "http://139.162.101.250/api/spyder_videos/videos?spyder_id=#{spyder_id}"
-
+    res = HTTParty.get "http://139.162.101.250/api/spyder_videos/videos?spyder_id=#{spyder_id}"
+    @data = JSON.parse(res.body)["data"]["videos"]
+    
     respond_to do |format|
       format.html
     end
