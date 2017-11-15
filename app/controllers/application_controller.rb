da38761c04be8b3ae9c92200becab3a5ce1f54d4
moumou <hielf@qq.com>
2017-11-15 17:30:57 +0800

    update yaml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include ApplicationHelper
+
+  def render_json(code, message)
+    render json: {code: code, message: message}
+  end
+
+  def error_404!
+    render_json(404, 'Page not found')
+  end
 end
--- app/controllers/wechatreports_controller.rb
+++ app/controllers/wechatreports_controller.rb
@@ -7,9 +7,23 @@ class WechatReportsController < ApplicationController
   # end
 
   def search_result
+    # openid = "og7qVxKXBQqtPAePsFSQGXqHWETk"
     openid = params[:open_id]
+    key_word = params[:key_word]
+    count = params[:count]
+
     template = YAML.load(File.read('app/views/templates/result.yml'))
+    template['template']['url'].gsub!("*url", "#{openid}")
+    template['template']['data']['first']['value'].gsub!("*first", "您好,已为您搜索到以下结果")
+    template['template']['data']['keyword1']['value'].gsub!("*keyword1", "#{key_word}")
+    template['template']['data']['keyword2']['value'].gsub!("*keyword2", "一共有#{count}条结果")
+    template['template']['data']['keyword3']['value'].gsub!("*keyword3", "")
+    template['template']['data']['keyword4']['value'].gsub!("*keyword4", "")
+    template['template']['data']['keyword5']['value'].gsub!("*keyword5", "")
+
     wechat.template_message_send Wechat::Message.to(openid).template(template['template'])
+
+    render_json(0, 'SUCCESS')
   end
 
 end
