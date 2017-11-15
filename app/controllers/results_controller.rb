1efc920cad5d46009d04649c3da3aedecd16d307
moumou <hielf@qq.com>
2017-11-16 04:51:59 +0800

    test

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,9 +7,9 @@ class ResultsController < ApplicationController
   end
 
   def show
-    respond_to do |format|
-      format.html
-    end
+    spyder_id = params[:spyder_id]
+    @res = HTTParty.get "http://139.162.101.250/api/spyder_videos/videos?spyder_id=#{spyder_id}"
+
   end
 
 end
