05f5fed0a4e4fbb92b1a110b460d8f8c1d031cc4
moumou <hielf@qq.com>
2017-12-01 01:49:06 +0800

    add secret

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,6 +7,9 @@ class VideosController < ApplicationController
     requires! :video_cover, type: String
     requires! :status, type: Integer, values: %w(0 1)
     requires! :secret, type: String
+
+    return render_json(1, '上传失败') if (params[:secret] != Digest::MD5.hexdigest (params[:name] + 1.to_s))
+
     video = Video.create(user_id: current_user.id,
                          name: params[:name],
                          video_src: params[:video_src],
