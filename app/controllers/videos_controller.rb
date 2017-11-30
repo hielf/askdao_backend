87e09d3f550e3795ee502ef450a456e4a56fc8b5
moumou <hielf@qq.com>
2017-12-01 02:45:45 +0800

    add user for video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,10 +7,11 @@ class VideosController < ApplicationController
     requires! :video_cover, type: String
     requires! :status, type: Integer, values: %w(0 1)
     requires! :secret, type: String
+    requires! :user, type: String
 
     return render_json(1, '上传失败') if (params[:secret] != (Digest::MD5.hexdigest (params[:name] + 1.to_s)))
 
-    video = Video.create(user_id: current_user.id,
+    video = Video.create(user_id: User.find_by(open_id: params[:user]).id,
                          name: params[:name],
                          video_src: params[:video_src],
                          video_cover: params[:video_cover],
